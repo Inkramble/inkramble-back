47f777715821f14be482e6b8b886b98e9bec39a5
rahanaman <104880000+rahanaman@users.noreply.github.com>
2025-10-21 00:37:51 +0900

    Initial Commit

---
+++ src/main/java/inkramble/InkrambleBackApplication.java
@@ -0,0 +1,13 @@
+package inkramble;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InkrambleBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InkrambleBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/inkramble/InkrambleBackApplicationTests.java
@@ -0,0 +1,13 @@
+package inkramble;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InkrambleBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
