2c46e1e2803134cf1745cec5bbd6965c1d2cca33
rahanaman <104880000+rahanaman@users.noreply.github.com>
2025-11-06 14:57:15 +0900

    Test : test property 연결

--- src/main/java/inkramble/dev/TestController.java
+++ src/main/java/inkramble/dev/TestController.java
@@ -1,19 +1,24 @@
-package inkramble;
+package inkramble.dev;
 
+import inkramble.config.DevProperties;
 import inkramble.filesystem.FileNode;
 import inkramble.filesystem.FileSystemService;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 
 @RestController
 public class TestController {
+    private final DevProperties devProperties;
+
+    public TestController(DevProperties devProperties) {
+        this.devProperties = devProperties;
+    }
+
     @GetMapping("/test")
     public FileNode getDirectory() throws IOException {
         FileSystemService fileSystemService = new FileSystemService();
-        return fileSystemService.readDirectory(TestConstants.getRootPath());
+        return fileSystemService.readDirectory(devProperties.getStorage().getRootPath());
     }
 }
